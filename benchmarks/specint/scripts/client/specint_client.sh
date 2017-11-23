252e0689a0a8b97459fa68be5c1f1317bb4fbad8
gadgilrajeev <rajeev.gadgil@cavium.com>
2017-11-23 04:27:27 -0800

    Removed white spaces

--- benchmarks/specint/scripts/client/specint_client.sh
+++ benchmarks/specint/scripts/client/specint_client.sh
@@ -165,7 +165,7 @@ START_SYS_MONITOR ${SYS_NAME}
 pushd /opt/cpu2017
 . shrc
 #runcpu -I --iterations=1 --noreportable --output_root=${PARENT}/${LOG_LOCATION} -c ${CONFIGPATH} --copies ${COPIES} ${TYPE}rate > ${PARENT}/${LOG_LOCATION}/${logfile} 2>&1
-cmd = "runcpu -I --iterations=1 --noreportable -c ${CONFIGPATH} --define gcc_dir=/opt/install --define num_core=${NUMCORES} --copies ${COPIES} ${TYPE}rate"
+cmd="runcpu -I --iterations=1 --noreportable -c ${CONFIGPATH} --define gcc_dir=/opt/install --define num_core=${NUMCORES} --copies ${COPIES} ${TYPE}rate"
 echo "`date -u` :: ${cmd}" >> cmdline.txt
 ${cmd} > ${PARENT}/${LOG_LOCATION}/${logfile} 2>&1
 mv results/* ${PARENT}/${LOG_LOCATION}
